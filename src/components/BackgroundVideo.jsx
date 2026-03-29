c266513ee5725f24b0104574fe857c7177d61ce1
Matias <matuprograma97@gmail.com>
2026-03-28 23:13:38 -0300

    Versión final con video optimizado

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -11,7 +11,7 @@ const BackgroundVideo = () => {
         playsInline
         className="video-background-element"
       >
-        <source src="https://drive.google.com/uc?export=download&id=1D4iM28DRq0icxWw54n8gAxbWqFVViKEp" type="video/mp4" />
+        <source src="/bg-coraline.mp4" type="video/mp4" />
       </video>
       <div className="video-overlay"></div>
     </div>
