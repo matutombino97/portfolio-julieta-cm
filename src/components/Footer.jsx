247d0682f694a9802beb80e79a1f295851284436
Matias <matuprograma97@gmail.com>
2026-03-31 20:49:33 -0300

    update LinkedIn link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
         <p>© {new Date().getFullYear()} Julieta Castillo. Desarrollado con mucho amor para la mejor CM del mundo.</p>
         <div className="footer-social">
           <a href="https://www.instagram.com/julieta_castillo_/?hl=es" target="_blank" rel="noopener noreferrer">Instagram</a>
-          <a href="#" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/julieta-castillo-9b644a307/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           <a href="https://wa.me/5492612451593" target="_blank" rel="noopener noreferrer">WhatsApp</a>
         </div>
       </div>
